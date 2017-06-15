90f3be4a09256702958b33d0f0f6eeec645a2ae1
Wing Liu <wing.liu12@imperial.ac.uk>
2017-06-15 23:05:44 +0800

    fixed printing messages for debugging

--- dataset/add_new_data.py
+++ dataset/add_new_data.py
@@ -4,10 +4,18 @@ import cv2
 import argparse
 import time
 import face_recognition as fr
+import tensorflow as tf
 
-data_dir = os.path.dirname(os.path.realpath(__file__))
+
+"""
+from __future__ import absolute_import
+data_dir_split = data_dir.split('/')
+sys.path.insert(0,''.join(data_dir_split[:len(data_dir_split)-2])+"/facenet/src/align")
+import align.detect_face as df
+"""
 
 def main(args):
+    data_dir = os.path.dirname(os.path.realpath(__file__))
     subdirs = [d for d in os.listdir('.') if os.path.isdir(d)]
     dest = data_dir+'/'+args.database_name
     if args.database_name not in subdirs:
@@ -16,19 +24,22 @@ def main(args):
     # 0:default camera, 1:external camera, can stubstitue with file_name for video clip   
     cam = cv2.VideoCapture(0)
     count = 0
+
+
     print "Recording..."
-    # Start reading from camera/video until the desired volume is met
     while count<args.volume and cam.isOpened():
 	_, frame = cam.read()
-	frame_small = cv2.resize(frame, (0,0), fx=args.size, fy=args.size)
+	frame_small = cv2.resize( frame, (0,0), fx=args.size, fy=args.size)
 	if (len(fr.face_locations(frame_small))>0):	
 	    serial = ['0']*5
 	    serial[len(serial)-len(str(count)):]=str(count)[:]
-	    cv2.imwrite(dest+'/'+args.subject_name+'_'.join(serial)+'.jpg',frame_small)
+	    cv2.imwrite(dest+'/'+args.subject_name+'_'+"".join(serial)+'.jpg',frame_small)
 	    count+=1
 	    time.sleep(0.1)
-	
+
     cam.release()
+    print "Done."
+    print "%s images were captured and stored in %s" % (args.volume, dest)
     
         
 def parse_arguments(argv):
@@ -45,4 +56,4 @@ if __name__ == '__main__':
     main(parse_arguments(sys.argv[1:]))
     end=time.time()
     volume = sys.argv[3] if len(sys.argv)>3	 else '50'
-    print "Finished : %s photos in %f seconds" % (volume,end-start)
+    print "Average: %f per photo" % ((end-start)/volume)
